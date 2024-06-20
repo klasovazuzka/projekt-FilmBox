bcf1ca759d4ddf48b2e20a5f339f3e8d56ca895a
klasovazuzka <159687810+klasovazuzka@users.noreply.github.com>
2024-06-20 20:09:45 +0200

    Update spolecne.js

--- spolecne.js
+++ spolecne.js
@@ -1,80 +1,17 @@
-/* Najde prvek s id 'menu-tlacitko'
-var menuTlacitko = document.querySelector('#menu-tlacitko');
+const menuTlacitkoElm = document.querySelector('#menu-tlacitko');
+const menuPolozkyElm = document.querySelector('#menu-polozky');
+const fasElm = document.querySelector('.fas');
 
-// Nyní můžeme s tímto prvkem dále pracovat
-if (menuTlacitko) {
-    // Příklad: Přidání event listeneru
-    menuTlacitko.addEventListener('click', function() {
-        console.log('Menu tlačítko bylo kliknuto');
-    });
+const toggleMenu = () => {
+	if (menuPolozkyElm.classList.contains('show')) {
+		menuPolozkyElm.classList.remove('show');
+		fasElm.classList.remove('fa-xmark');
+		fasElm.classList.add('fa-bars');
+	} else {
+		menuPolozkyElm.classList.add('show');
+		fasElm.classList.add('fa-xmark');
+		fasElm.classList.remove('fa-bars');
+	}	
 }
 
-// Najde prvek s id 'menu-tlacitko'
-var menuTlacitko = document.querySelector('#menu-tlacitko');
-
-// Přidání posluchače události kliknutí
-if (menuTlacitko) {
-    menuTlacitko.addEventListener('click', function() {
-        console.log('Menu tlačítko bylo kliknuto');
-        // Další akce, které chcete provést při kliknutí na tlačítko
-        // Například změna třídy, otevření menu apod.
-    });
-}
-
-// Najde prvek s id 'menu-tlacitko'
-var menuTlacitko = document.querySelector('#menu-tlacitko');
-
-// Najde prvek s id 'menu-polozky'
-var menuPolozky = document.querySelector('#menu-polozky');
-
-// Přidání posluchače události kliknutí
-if (menuTlacitko && menuPolozky) {
-    menuTlacitko.addEventListener('click', function() {
-        console.log('Menu tlačítko bylo kliknuto');
-        // Přidání třídy 'show' prvku 'menu-polozky'
-        menuPolozky.classList.add('show');
-    });
-}
-
-// Najde prvek s id 'menu-tlacitko'
-var menuTlacitko = document.querySelector('#menu-tlacitko');
-
-// Najde prvek s id 'menu-polozky'
-var menuPolozky = document.querySelector('#menu-polozky');
-
-// Přidání posluchače události kliknutí
-if (menuTlacitko && menuPolozky) {
-    menuTlacitko.addEventListener('click', function() {
-        console.log('Menu tlačítko bylo kliknuto');
-        // Přidání nebo odebrání třídy 'show' prvku 'menu-polozky'
-        if (menuPolozky.classList.contains('show')) {
-            menuPolozky.classList.remove('show');
-        } else {
-            menuPolozky.classList.add('show');
-        }
-    });
-}*/
-
-// Najde prvek s id 'menu-tlacitko'
-var menuTlacitko = document.querySelector('#menu-tlacitko');
-
-// Najde prvek s id 'menu-polozky'
-var menuPolozky = document.querySelector('#menu-polozky');
-
-// Přidání posluchače události kliknutí
-if (menuTlacitko && menuPolozky) {
-    menuTlacitko.addEventListener('click', function() {
-        console.log('Menu tlačítko bylo kliknuto');
-        
-        // Přidání nebo odebrání třídy 'show' prvku 'menu-polozky'
-        if (menuPolozky.classList.contains('show')) {
-            menuPolozky.classList.remove('show');
-            // Změna ikonky na hamburger
-            menuTlacitko.innerHTML = '<i class="fas fa-bars"></i>';
-        } else {
-            menuPolozky.classList.add('show');
-            // Změna ikonky na křížek
-            menuTlacitko.innerHTML = '<i class="fas fa-xmark"></i>';
-        }
-    });
-}
+menuTlacitkoElm.addEventListener('click', toggleMenu);
